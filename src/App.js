54891f14cc4edc4a3fc3589430b1cc026610fd3b
GOURAV0188 <guptagourav0306@gmail.com>
2022-12-18 19:17:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 
 
   <Route  path="/login"  element={<Login />} />
-  <Route  path="/register5"  element={<Register/>} />
+  <Route  path="/register"  element={<Register/>} />
 
 
 <Route path="/"  element={<ProtectedRoute />} > 
